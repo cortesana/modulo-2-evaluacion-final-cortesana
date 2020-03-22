c1a27ce93f000274848d6baef7d9f2308016f8ae
cortesana <a.chdez2020@gmail.com>
2020-03-22 18:50:23 +0100

    Add fetch data from API and display

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const searchForm = document.querySelector('#searchForm');
+const titleInput = document.querySelector('#titleInput.value');
+const searchBtn = document.querySelector('#searchBtn');
+const baseURL = 'http://api.tvmaze.com/search/shows?q=';
+const ulElem = document.querySelector('#ulElem');
+let contentList = [];
+
+function getInfo() {
+    ulElem.innerHTML = '';
+    fetch(`${baseURL}`+ titleInput)
+        .then(response => response.json())
+        .then(data => {
+            contentList = data;
+            displayInfo(contentList);
+        })
+}
+
+function displayInfo(contentList) {
+    for (let i = 0; i < contentList.length; i++) {
+        if(contentList[i].show.image !== null) {
+            ulElem.innerHTML += `<li><img src='${contentList[i].show.image.medium}' alt="Content cover"><h2>${contentList[i].show.name}</h2></li>`;
+        } else {
+            ulElem.innerHTML += `<li><img src='https://via.placeholder.com/210x295/ffffff/666666/?text=TV' alt="Content cover"><h2>${contentList[i].show.name}</h2></li>`;
+        }
+    }
+}
+    
+getInfo();
