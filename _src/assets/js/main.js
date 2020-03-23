9a56861ccec008c1c909acb8aa6e703f2ff0351f
cortesana <a.chdez2020@gmail.com>
2020-03-23 13:31:33 +0100

    New problems with API, still not working

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const searchForm = document.querySelector('#searchForm');
 const inputSearchBar = document.querySelector('#titleInput');
 const searchBtn = document.querySelector('#searchBtn');
 const ulElem = document.querySelector('#ulElem');
+const favouritesList = document.querySelector('.favouritesList');
 const baseURL = 'http://api.tvmaze.com/search/shows?q=';
 let contentList = [];
-
+let selectedFavourites = readLocalStorage();
 
 function getInfo() {
     fetch(baseURL+inputSearchBar.value)
@@ -17,16 +17,16 @@ function getInfo() {
         })
 }
 
-function displayInfo(contentList) {
+function displayInfo(array) {
     ulElem.innerHTML = '';
-    for(let item of contentList){
-        alert('hola');
-        if(item.show.image === null) {
-            ulElem.innerHTML += `<li id=${item.show.id}><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"><h2 id="tvShowTitle">${item.show.name}</h2></li>`
+    for(let item of array){
+        if(item.show.image !== null) {
+            ulElem.innerHTML += `<li class="tvShowsList defaultColor" id=${item.show.id}><img src=${item.show.image.medium}><h2 id="tvShowTitle">${item.show.name}</h2></li>`
         } else {
-            ulElem.innerHTML += `<li id=${item.show.id}><img src=${item.show.image.medium}><h2 id="tvShowTitle">${item.show.name}</h2></li>`
+            ulElem.innerHTML += `<li class="tvShowsList defaultColor" id=${item.show.id}><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"><h2 id="tvShowTitle">${item.show.name}</h2></li>`
         }
-    }
+    } 
+        addListenersToTVShows();
 }
 
 searchBtn.addEventListener('click', getInfo);
