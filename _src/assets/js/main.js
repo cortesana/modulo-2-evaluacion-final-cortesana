688831b7329400ceec6aad02e4667c80929513a8
cortesana <a.chdez2020@gmail.com>
2020-03-23 14:43:25 +0100

    Favourites working, 'This film is alredy your favourite' not working

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,6 +6,7 @@ const ulElem = document.querySelector('#ulElem');
 const favouritesList = document.querySelector('.favouritesList');
 const baseURL = 'http://api.tvmaze.com/search/shows?q=';
 let contentList = [];
+let selectedFavourites = readLocalStorage();
 
 function getInfo() {
     fetch(baseURL+inputSearchBar.value)
@@ -28,5 +29,69 @@ function displayInfo(array) {
         addListenersToTVShows();
 }
 
+function addListenersToTVShows() {
+    const tvShowsList = document.querySelectorAll('.tvShowsList');
+    for(let liItem of tvShowsList) {
+        liItem.addEventListener('click', addToFavourites);
+    }
+}
+
+function addToFavourites(event) {
+    event.currentTarget.classList.remove('defaultColor');
+    event.currentTarget.classList.add('colorFavourites');
+    const currentItem = event.currentTarget.id;
+    const currentItemIndex = selectedFavourites.indexOf(currentItem);
+    console.log(currentItemIndex);
+    console.log(selectedFavourites);
+    const favouriteObj = getFavouriteObj(currentItem);
+    if(selectedFavourites.indexOf(currentItem) === -1){
+        selectedFavourites.push(favouriteObj.show);
+        setLocalStorage();
+        printFavourites(selectedFavourites);
+    } else {
+        alert('This film is alredy your favourite');
+    }
+}
+
+function setLocalStorage() {
+    localStorage.setItem('myFavourites', JSON.stringify(selectedFavourites));
+}
+
+function readLocalStorage(){
+    let localFavourites = JSON.parse(localStorage.getItem('myFavourites'));
+    if(localFavourites !== null){
+        return localFavourites;
+    }else{
+    return localFavourites = [];
+    }
+}
+
+function getFavouriteObj(id){
+    return contentList.find(item => item.show.id === parseInt(id));
+}
+
+
+function printFavourites(arrayFav){
+    favouritesList.innerHTML = '';
+    for(let favouriteItem of arrayFav){
+        favouritesList.innerHTML+= `<li class ="FavouriteListItem" id=${favouriteItem.id}><img src=${favouriteItem.image.medium}><button type="button" class="rmBtn">Delete</button><h3 class="titleFavourite">${favouriteItem.name}</h3></li>`
+        addRemoveListeners();
+    }
+    
+}
 searchBtn.addEventListener('click', getInfo);
 
+function addRemoveListeners(){
+    const rmFavouritesButton = document.querySelectorAll('.rmBtn')
+    for(let btnItem of rmFavouritesButton){
+        btnItem.addEventListener('click', removeFavourite);
+    }
+}
+
+function removeFavourite(){
+    const fullElementId = event.currentTarget.parentElement.id;
+    const elemIndex = selectedMovies.indexOf(fullElementId);
+    selectedFavourites.splice(elemIndex,1);
+    setLocalStorage();
+    printFavourites(selectedFavourites);
+}
